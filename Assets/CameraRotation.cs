573e780b99072db07cc2e9fa727f6e3ad954ace3
Gajam_Jyothsna <jyothsna.gajam@yesgnome.com>
2022-04-21 21:48:31 +0530

    Camera Rotation is fixed.

--- Assets/CameraRotation.cs
+++ Assets/CameraRotation.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class CameraRotation : MonoBehaviour
 {
+    public float rotationSpeed;
+   
     // Start is called before the first frame update
     void Start()
     {
-        transform.rotation = Quaternion.Euler(0,90,0) * Quaternion.Euler(0,30,0) ;
+      
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+        transform.rotation = transform.rotation * Quaternion.Euler(0, mouseX*rotationSpeed, 0); //Gives Orientation
+        Camera cam = GetComponentInChildren<Camera>();
+        cam.transform.localRotation = Quaternion.Euler(-mouseY, 0, 0) * cam.transform.localRotation;
+
+
+
+
+
+
     }
 }
